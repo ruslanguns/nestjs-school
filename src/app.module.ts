c47baa62fa2a675e77179ad4280578bcae51b48c
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-08-30 02:14:06 +0100

    config module added and removed local db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,34 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
 import { Student } from './student/student.entity';
 import { StudentModule } from './student/student.module';
 import { UpperCaseDirective } from './shared/directives';
+import { DATABASE_CONFIG } from './config/constants';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost/school',
-      synchronize: true,
-      useUnifiedTopology: true,
-      entities: [
-        Lesson,
-        Student
-      ]
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mongodb',
+        url: config.get<string>(DATABASE_CONFIG),
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [
+          Lesson,
+          Student
+        ]
+      })
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
---
+++ src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config'
+
+export default registerAs('database', () => ({
+  config: createMongoURI()
+}));
+
+const createMongoURI = (): string => {
+  const host = process.env.DB_HOST;
+  const username = process.env.DB_USERNAME;
+  const name = process.env.DB_NAME;
+  const password = process.env.DB_PASSWORD;
+  return `mongodb+srv://${username}:${password}@${host}/${name}?retryWrites=true&w=majority`
+}
