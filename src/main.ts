82ae9693a502fbb177fab87488786abefa5a8e63
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-08-30 12:23:00 +0100

    fix port from ENV

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = app.get<string>('PORT')
+  const config = app.get(ConfigService);
+  const logger = new Logger('Bootstrap');
+  const port = config.get<number>(SERVER_PORT);
 
   app.useGlobalPipes(
     new ValidationPipe()
   );
 
   await app.listen(port);
+  logger.log(`Server is running at ${await app.getUrl()}`)
 }
 bootstrap();
